349301f229ef55bf91644a83528c23e2576006ed
Jesus Alcantara <jesus.alcantara@alten.es>
2023-02-14 12:54:25 +0100

    Commit all

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    name: [ 'Test 4', Validators.required ],
+    email: [ 'test4@test.com', [Validators.required, Validators.email] ],
+    password: [ '123456', [Validators.required, Validators.minLength(6)] ]
+  });
 
-  ngOnInit(): void {
+  constructor( private fb: FormBuilder, private router: Router, private authService: AuthService ) { }
+
+  register () {
+    const {name, email, password} = this.miFormulario.value;
+    this.authService.register(name, email, password)
+      .subscribe( ok => {
+        if ( ok === true ) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error');
+        }
+      })
   }
-
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,74 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { catchError, map, of, tap, Observable } from 'rxjs';
+import { AuthResponse, Usuario } from '../interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _baseUrl: string = environment.baseUrl;
+  private _usuario!: Usuario;
+
+  get usuario () {
+    return { ...this._usuario };
+  }
+
+  constructor( private http: HttpClient ) { }
+
+  login ( email: string, password: string ) {
+    const url = `${this._baseUrl}/auth`;
+    const body = { email, password };
+    return this.http.post<AuthResponse>(url, body)
+      .pipe(
+        tap( resp => {
+          if ( resp.ok ) {
+            localStorage.setItem('token', resp.token!);
+          }
+        }),
+        map( resp => resp.ok ),
+        catchError( err => of(err.error.msg) )
+      );
+  }
+
+  register ( email: string, name: string, password: string ) {
+    const url = `${this._baseUrl}/auth/new`;
+    const body = { email, password, name };
+    return this.http.post<AuthResponse>(url, body)
+      .pipe(
+        tap( ({ok, token}) => {
+          if ( ok ) {
+            localStorage.setItem('token', token!);
+          }
+        }),
+        map( resp => resp.ok ),
+        catchError( err => of(err.error.msg) )
+      );
+  }
+
+  validarToken (): Observable<boolean> {
+    const url = `${this._baseUrl}/auth/renew`;
+    const headers = new HttpHeaders()
+      .set('x-token', localStorage.getItem('token') || '');
+    return this.http.get<AuthResponse>(url, { headers: headers })
+      .pipe(
+        map( resp => {
+          localStorage.setItem('token', resp.token!);
+            this._usuario = {
+              name: resp.name!,
+              uid: resp.uid!,
+              email: resp.email!
+          }
+          return resp.ok!;
+        }),
+        catchError( err => of(false) )
+      );
+  }
+
+  logout () {
+    localStorage.clear();
+  }
+
+}
